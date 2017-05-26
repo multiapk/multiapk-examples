021699f80674cfe9855ff9a1ff5c5711fe4e84f9
krmao <krmao@ctrip.com>
2017-05-26 23:21:44 +0800

    add auto value and remove sqlbrite

--- android/app/examples/src/main/java/com/multiapk/modules/home/HomeActivity.java
+++ android/app/examples/src/main/java/com/multiapk/modules/home/HomeActivity.java
@@ -3,7 +3,6 @@ package com.multiapk.modules.home;
 import android.Manifest;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.jakewharton.rxbinding2.view.RxView;
 import com.multiapk.R;
@@ -16,10 +15,7 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 
-public class HomeActivity extends MFragmentActivity /*implements EasyPermissions.PermissionCallbacks */ {
-
-    private RxPermissions rxPermissions;
-
+public class HomeActivity extends MFragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,15 +28,9 @@ public class HomeActivity extends MFragmentActivity /*implements EasyPermissions
             }
         });
 
-        findViewById(R.id.cardViewMobileModule).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MCommonActivity.start(HomeActivity.this, "com.multiapk.modules.mobile.MobileFragment");
-            }
-        });
+        findViewById(R.id.cardViewMobileModule).setOnClickListener(view -> MCommonActivity.start(HomeActivity.this, "com.multiapk.modules.mobile.MobileFragment"));
 
-
-        rxPermissions = new RxPermissions(this);
+        RxPermissions rxPermissions = new RxPermissions(this);
         rxPermissions.request(Manifest.permission.SEND_SMS, Manifest.permission.READ_CONTACTS)
                 .subscribe(granted -> {
                     if (granted) {
@@ -56,7 +46,7 @@ public class HomeActivity extends MFragmentActivity /*implements EasyPermissions
                 .subscribe(permission -> { // will emit 2 Permission objects
                     if (permission.granted) {
                         // `permission.name` is granted !
-                        Log.d("krmao", "permission.name` is granted");
+                        Log.d("krmao", "permission:" + permission.name + " is granted");
                     } else if (permission.shouldShowRequestPermissionRationale) {
                         // Denied permission without ask never again
                         Log.d("krmao", "Denied permission without ask never again");
@@ -67,51 +57,5 @@ public class HomeActivity extends MFragmentActivity /*implements EasyPermissions
                         Log.d("krmao", "Need to go to the settings");
                     }
                 });
-
-        //EasyPermissions.requestPermissions(this, "我们需要相机和录音权限1", RC_CAMERA_AND_LOCATION, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO);
-        //Log.d("krmao", "requestPermissions 执行请求权限 CAMERA + RECORD_AUDIO");
     }
-
-    /*@Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Forward results to EasyPermissions
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-        Log.d("krmao", "onRequestPermissionsResult");
-        for (int i = 0; i < permissions.length; i++) {
-            Log.w("krmao", "onRequestPermissionsResult: permissions[" + i + "]=" + permissions[i] + " , grantResults[" + i + "]=" + grantResults[i]);
-        }
-    }
-
-    public static final int RC_CAMERA_AND_LOCATION = 777;
-
-    @AfterPermissionGranted(RC_CAMERA_AND_LOCATION)
-    private void methodRequiresTwoPermission() {
-        Log.d("krmao", "methodRequiresTwoPermission");
-        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
-        if (EasyPermissions.hasPermissions(this, perms)) {
-            // Already have permission, do the thing
-            // ...
-            Log.d("krmao", "methodRequiresTwoPermission: 已经获取全部权限");
-        } else {
-            // Do not have permissions, request them now
-            Log.d("krmao", "methodRequiresTwoPermission: 有权限尚未获取");
-            EasyPermissions.requestPermissions(this, "我们需要相机和录音权限2", RC_CAMERA_AND_LOCATION, perms);
-        }
-    }
-
-    @Override
-    public void onPermissionsGranted(int requestCode, List<String> perms) {
-        Log.d("krmao", "onPermissionsGranted:" + requestCode + ":" + perms.toString());
-    }
-
-    @Override
-    public void onPermissionsDenied(int requestCode, List<String> perms) {
-        Log.d("krmao", "onPermissionsGranted:" + requestCode + ":" + perms.toString());
-        // (Optional) Check whether the user denied any permissions and checked "NEVER ASK AGAIN."
-        // This will display a dialog directing them to enable the permission in app settings.
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            new AppSettingsDialog.Builder(this).build().show();
-        }
-    }*/
 }
